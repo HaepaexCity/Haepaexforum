adb1b8e7944f9165eb0509fbf580fc862c1ce3c5
Haepaexcity <kinggold.lego@gmail.com>
2020-08-28 02:54:21 +0100

    more changes

---
+++ Haepaexsite/js tutor/test.js
@@ -0,0 +1,12 @@
+function multiply(x, y) {
+	return x * y;
+};
+console.log(multiply(5, 3));
+
+function divide(x, y, z) {
+	return x * y / z
+};
+console.log(divide(3, 7, 5));
+
+var a = 5;
+console.log(a);
